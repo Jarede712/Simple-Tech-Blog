760b0133b40ccf4406bf4bf2e269270098bd296b
Jarede712 <jaredeic@yahoo.com>
2024-01-23 00:24:14 -0800

    fixed issue with config

--- models/index.js
+++ models/index.js
@@ -1,5 +1,5 @@
 const Sequelize = require("sequelize");
-const sequelize = require("../config/connection");
+const sequelize = require("../config/config");
 
 // Import models
 const User = require("./User");
--- server.js
+++ server.js
@@ -3,9 +3,7 @@ const session = require("express-session");
 const exphbs = require("express-handlebars");
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
-const routes = require("./controllers");
-
-const sequelize = require("./config/connection");
+const sequelize = require("./config/config");
 const routes = require("./controllers"); // Ensure you have your routes set up in the controllers directory
 const helpers = require("./utils/helpers"); // Optional: Include if you have handlebars helpers
 
