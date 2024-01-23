479e1562fac1b500127b7cbcc7589f6b83a633a8
Jarede712 <jaredeic@yahoo.com>
2024-01-22 23:46:37 -0800

    defined all models and setup relationships using the index.js file

---
+++ models/index.js
@@ -0,0 +1,41 @@
+const Sequelize = require("sequelize");
+const sequelize = require("../config/connection");
+
+// Import models
+const User = require("./User");
+const Post = require("./Post");
+const Comment = require("./Comment");
+
+// Associations
+User.hasMany(Post, {
+  foreignKey: "user_id",
+  onDelete: "CASCADE", // If a user is deleted, delete all their posts
+});
+
+Post.belongsTo(User, {
+  foreignKey: "user_id",
+  onDelete: "CASCADE", // If a user is deleted, delete all their posts
+});
+
+Post.hasMany(Comment, {
+  foreignKey: "post_id",
+  onDelete: "CASCADE", // If a post is deleted, delete all its comments
+});
+
+Comment.belongsTo(User, {
+  foreignKey: "user_id",
+  onDelete: "CASCADE", // If a user is deleted, delete all their comments
+});
+
+Comment.belongsTo(Post, {
+  foreignKey: "post_id",
+  onDelete: "CASCADE", // If a post is deleted, delete all its comments
+});
+
+// Export models and sequelize connection
+module.exports = {
+  sequelize,
+  User,
+  Post,
+  Comment,
+};
